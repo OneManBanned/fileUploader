fa9b1a8a482a51ad04e3cd153a13b018affe13b6
Brendan <brendanlawless@hotmail.com>
2024-11-28 11:04:16 +0000

    chore: create express server

---
+++ app.js
@@ -0,0 +1,8 @@
+import express from "express"
+
+const PORT = 3000
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello App"))
+
+app.listen(PORT, () => console.log(`listening on http//localhost:${PORT}`))
