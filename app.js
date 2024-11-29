daebcd15ca08e2f25ae2175838ce2ff96333f438
Brendan <brendanlawless@hotmail.com>
2024-11-29 10:43:36 +0000

    feat: integrate session, passport, prisma & ejs into main app

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 import express from "express"
+import ejs from "ejs"
+import session from "./config/session/session.js";
+import passport from "passport";
 
 const PORT = 3000
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello App"))
+app.engine("html", ejs.renderFile);
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(session)
+import "./config/passport/passport.js";
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", (req, res) => res.render("index.html"))
 
 app.listen(PORT, () => console.log(`listening on http//localhost:${PORT}`))
