07ae6564aca34392f2d219bdb70165bde01e96d2
Brendan <brendanlawless@hotmail.com>
2024-11-29 20:55:19 +0000

    feat[login]: add login functionality

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import loginController from "../controllers/loginController.js";
+import passport from "passport";
 
 const loginRoute = Router();
 
 loginRoute.get("/", loginController.get)
+loginRoute.post("/", passport.authenticate('local', {failureRedirect: '/', successRedirect: '/'}))
 
 export default loginRoute;
