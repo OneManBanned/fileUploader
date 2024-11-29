0731b24e6a077acf3507e8ba92876c87525464ec
Brendan <brendanlawless@hotmail.com>
2024-11-29 20:58:43 +0000

    feat[logout]: add route and controller for logout

---
+++ controllers/logoutController.js
@@ -0,0 +1,13 @@
+const logoutController = {
+    get: (req, res, next) => {
+        req.logout(function(err) {
+            if (err) {
+                return next(err)
+            }
+
+            res.redirect("/")
+        }) 
+    }
+}
+
+export default logoutController;
