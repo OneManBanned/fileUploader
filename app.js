1ae72ddccb0508a4f50f167e3141ed47399fe71d
Brendan <brendanlawless@hotmail.com>
2024-11-30 11:38:18 +0000

    feat: add routes
    - add upload route
    - add logout route
    - add error catcher route

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ import ejs from "ejs"
 import session from "./config/session/session.js";
 import registerRoute from "./routes/registerRoute.js";
 import loginRoute from "./routes/loginRoute.js";
+import logoutRoute from "./routes/logoutRoute.js";
+import uploadRoute from "./routes/uploadRoute.js";
 import passport from "passport";
+import { isAuth } from "./utils/isAuth.js";
 
 const PORT = 3000
 const app = express();
@@ -11,7 +14,6 @@ const app = express();
 app.engine("html", ejs.renderFile);
 
 app.use(express.urlencoded({ extended: true }));
-
 
 app.use(session)
 import "./config/passport/passport.js";
@@ -21,7 +23,11 @@ app.use(passport.session());
 
 app.use("/register", registerRoute)
 app.use("/login", loginRoute)
-
-app.get("/", (req, res) => res.render("index.html"))
+app.use("/logout", logoutRoute)
+app.use("/upload", isAuth, uploadRoute)
+app.get("/", (req, res) => res.render("index.html", {user: req.user}))
+app.use((err, req, res, next) => {
+    console.error(err)
+})
 
 app.listen(PORT, () => console.log(`listening on http//localhost:${PORT}`))
