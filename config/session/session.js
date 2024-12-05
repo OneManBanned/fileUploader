7f361e537f143bd718647478e440d3148ded8c0b
Brendan <brendanlawless@hotmail.com>
2024-12-05 16:58:26 +0000

    build[db]: move db clients into individual folders

--- config/session/session.js
+++ config/session/session.js
@@ -1,17 +1,19 @@
 import exportSession from "express-session";
 import { PrismaSessionStore } from "@quixo3/prisma-session-store";
-import prisma from "../prismaClient/client.js";
+import prisma from "../prismaClient/prismaClient.js";
 
 const session = exportSession({
-    cookie: {
-        maxAge: 24 * 60 * 60 * 1000 
-    },
-    secret: process.env.SESSIONSECRET,
-    resave: true,
-    saveUninitialized: true,
-    store: new PrismaSessionStore( prisma, { checkPeriod: 2 * 60 * 1000, dbRecordIdIsSessionId: true, dbRecordIdFunction: undefined,
-        }
-    )
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+  },
+  secret: process.env.SESSIONSECRET,
+  resave: true,
+  saveUninitialized: true,
+  store: new PrismaSessionStore(prisma, {
+    checkPeriod: 2 * 60 * 1000,
+    dbRecordIdIsSessionId: true,
+    dbRecordIdFunction: undefined,
+  }),
 });
 
 export default session;
